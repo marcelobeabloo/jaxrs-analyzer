4df5cf9ec51b2d6f0f5ff218b063840a7a4f3c18
marcelobeabloo <marcelo.magalhaes@beabloo.com>
2019-03-21 17:14:02 +0100

    rename package

---
+++ src/main/java/com/sebastian_daschner/jaxrs_analyzer/analysis/javadoc/ResponseCommentExtractor.java
@@ -0,0 +1,34 @@
+package com.sebastian_daschner.jaxrs_analyzer.analysis.javadoc;
+
+import com.sebastian_daschner.jaxrs_analyzer.LogProvider;
+import com.sebastian_daschner.jaxrs_analyzer.utils.Pair;
+
+class ResponseCommentExtractor {
+
+    private ResponseCommentExtractor() {
+    }
+
+    static final String RESPONSE_TAG_NAME = "response";
+    static final String RETURN_TAG_NAME = "return";
+
+    static Pair<Integer, String> extract(String comment) {
+        try {
+            String commentText = comment.trim();
+            String statusPart = commentText.split("\\s")[0];
+            int status = Integer.parseInt(statusPart);
+            return Pair.of(status, commentText.substring(statusPart.length()).trim());
+        } catch (Exception e) {
+            LogProvider.info("Warning: malformed @response JavaDoc tag: '@response " + comment + "'");
+            LogProvider.debug(e);
+            return extractByReturnAnnotation(comment);
+        }
+    }
+
+    static Pair<Integer, String> extractByReturnAnnotation(String comment) {
+        String commentText = comment.trim();
+        int status = 200;
+        return Pair.of(status, commentText);
+    }
+
+
+}
---
+++ src/test/java/com/sebastian_daschner/jaxrs_analyzer/analysis/classes/testclasses/resource/object/TestClass6.java
@@ -0,0 +1,31 @@
+package com.sebastian_daschner.jaxrs_analyzer.analysis.classes.testclasses.resource.object;
+
+import com.sebastian_daschner.jaxrs_analyzer.model.elements.HttpResponse;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class TestClass6 {
+
+    @javax.ws.rs.GET public List<Model> method() {
+        if ("".equals(""))
+            return new ArrayList<>();
+        return new LinkedList<>();
+    }
+
+    public static Set<HttpResponse> getResult() {
+        final HttpResponse result = new HttpResponse();
+        result.getEntityTypes().add("Ljava/util/List<Lcom/sebastian_daschner/jaxrs_analyzer/analysis/classes/testclasses/resource/object/TestClass6$Model;>;");
+
+        return Collections.singleton(result);
+    }
+
+    private class Model {
+        public Model(final String string) {
+        }
+    }
+
+}
